16cbf03d163f811d35b38d5024a62f00e42bcadc
Harmanjot kaur <jotharman712@gmail.com>
2026-06-11 18:11:16 +0530

    fix navbar import case

--- src/pages/public/Landing.jsx
+++ src/pages/public/Landing.jsx
@@ -1,4 +1,4 @@
-import Navbar from "../../components/ Navbar"
+import Navbar from "../../components/Navbar"
 import HeroSection from '../../components/HeroSection';
 import FeaturesSection from '../../components/FeaturesSection';
 import HowItWorks from '../../components/HowItWorks';
