4f70ce19e4e6c36162c44ae195754920d9edb794
Harmanjot kaur <jotharman712@gmail.com>
2026-06-18 10:52:56 +0530

    Fix import - use chatAPI instead of chatApi

--- src/components/UserChatPage.jsx
+++ src/components/UserChatPage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { chatApi } from '../api/chatApi.js';
+import { chatAPI } from '../../api/chatApi.js';
 import { initSocket, joinChat, sendMessage, onReceiveMessage } from '../services/socketService.js';
 import { MessageSquare } from 'lucide-react';
 import EmojiPicker from 'emoji-picker-react';
