def1fad11cd04be90cc5c5c4f1247e36aac4e097
Harmanjot kaur <jotharman712@gmail.com>
2026-06-19 12:28:46 +0530

    redo app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,4 @@ import './App.css';
 function App() {
   return <AppRoutes />;
 }
-export default function App() {
-  console.log("APP LOADED");
-
-  return <div style={{ color: "white" }}>APP WORKING</div>;
-}
+export default App;
